8983a613f7838b6c2f009598071c966bc4f25204
Wallace Vidal <wallacevff@hotmail.com>
2019-12-11 16:49:29 -0300

    Exercicio Conversor de Moeda

---
+++ Course/Course/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course {
+    static class ConversorDeMoeda {
+        static double IOF = 0.06;
+        static public double Converter(double moeda, double cotacao) {
+            return (moeda * cotacao) + IOF * (moeda * cotacao);
+        }
+    }
+}
---
+++ Course/Course/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            double moeda, dolar;
+
+            Console.Write("Qual é a cotação do dólar? ");
+            moeda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Qauntos dólares você vai comprar? ");
+            dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor pago em reais " + ConversorDeMoeda.Converter(moeda,dolar).ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}

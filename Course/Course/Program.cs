cf0b5fb79a94fd18ba7f39adbf594718eb757dd0
Wallace Vidal <wallacevff@hotmail.com>
2019-12-11 17:08:44 -0300

    Programa Produto

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -4,16 +4,28 @@ using System.Globalization;
 namespace Course {
     class Program {
         static void Main(string[] args) {
-            double moeda, dolar;
-
-            Console.Write("Qual é a cotação do dólar? ");
-            moeda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.Write("Qauntos dólares você vai comprar? ");
-            dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            Console.WriteLine("Valor pago em reais " + ConversorDeMoeda.Converter(moeda,dolar).ToString("F2",CultureInfo.InvariantCulture));
-
+            Produto p = new Produto();
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
         }
     }
 }

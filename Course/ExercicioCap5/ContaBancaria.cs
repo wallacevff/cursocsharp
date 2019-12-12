23c952c28e0a6874be4825d146ec76df960e406c
Wallace Vidal <wallacevff@hotmail.com>
2019-12-12 10:27:03 -0300

    Exercício de Fixação Cap5

---
+++ Course/ExercicioCap5/ContaBancaria.cs
@@ -0,0 +1,61 @@
+﻿using System.Globalization;
+
+namespace ExercicioCap5 {
+    class ContaBancaria {
+        private string _nome;
+        private double _saldo;
+        public int NumeroConta { get; }
+        private static int desconto = 5;
+
+        public ContaBancaria(int numeroConta, string nome) {
+            if (numeroConta > 0) {
+                NumeroConta = numeroConta;
+            }
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+
+        public ContaBancaria(int numeroConta, string nome, double deposito) : this(numeroConta, nome) {
+
+            if (deposito > 0) {
+                _saldo = deposito;
+            }
+
+        }
+
+
+        public string Nome {
+            set {
+                if (value != null && value.Length > 1) {
+                    _nome = value;
+                }
+            }
+            get { return _nome; }
+        }
+
+
+        public void Saque(double saque) {
+            if (saque > 0) {
+                _saldo -= (saque + desconto);
+            }
+        }
+
+        public void Deposito(double deposito) {
+            if (deposito > 0) {
+                _saldo += deposito;
+            }
+        }
+
+        public override string ToString() {
+            return "Conta: " +
+                    NumeroConta.ToString() +
+                    ", Titular: " +
+                    Nome +
+                    ", Saldo: $ " +
+                    _saldo.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ Course/ExercicioCap5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioCap5 {
+    class Program {
+        static void Main(string[] args) {
+            ContaBancaria cb;
+            string nome, dep;
+            int numero;
+            double movimento;
+
+            Console.Write("Entre com o número da conta: "); numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: "); nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n): "); dep = Console.ReadLine();
+
+            if (dep == "s") {
+                Console.Write("Entre com o valor de depósio inicial: ");
+                movimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cb = new ContaBancaria(numero, nome, movimento);
+            } else {
+                cb = new ContaBancaria(numero, nome);
+            }
+
+            Console.WriteLine("\nDados da conta:");
+            Console.WriteLine(cb);
+
+            Console.Write("Entre com um valor de depósito: "); 
+            cb.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("\nDados Atualizados:\n" + cb);
+
+            Console.Write("\nEntre com um valor de saque: ");
+            cb.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("\nDados Atualizados:\n" + cb);
+
+
+        }
+    }
+}

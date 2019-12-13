49f0bbf9bec1be77dd270a8cde43ff6d9b18058d
Wallace Vidal <wallacevff@hotmail.com>
2019-12-13 12:08:37 -0300

    Aula 72 Modificador de Parametros

---
+++ Course/Aula72ModificadorParametros/Calculaor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula72ModificadorParametros {
+    class Calculaor {
+
+        public static int Sum(params int[] args) {
+            int sum = 0;
+            for(int i = 0; i < args.Length; i++) {
+                sum += args[i];
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ Course/Aula72ModificadorParametros/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Aula72ModificadorParametros {
+    class Program {
+        static void Main(string[] args) {
+            int soma = 0;
+            soma = Calculaor.Sum( 2, 3, 4 );
+            Console.WriteLine(soma);
+        }
+    }
+}

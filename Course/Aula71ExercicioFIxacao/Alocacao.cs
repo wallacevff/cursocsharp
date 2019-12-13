fb00dcaac06a652e26eef0b901c150ebace5d9b1
Wallace Vidal <wallacevff@hotmail.com>
2019-12-13 11:55:14 -0300

    Exercicio Aula 71 Resolvido

---
+++ Course/Aula71ExercicioFIxacao/Alocacao.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Aula71ExercicioFIxacao {
+    class Alocacao {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public override string ToString() {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ Course/Aula71ExercicioFIxacao/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula71ExercicioFIxacao {
+    class Program {
+        static void Main(string[] args) {
+            int numeroapt, n;
+            string nome, email;
+            Console.Write("How many will be rented? ");
+            n = int.Parse(Console.ReadLine());
+            Alocacao[] a = new Alocacao[10];
+
+            for (int i = 1; i <= n; i++) {
+                Console.Write("\nRent #" + i.ToString() + ":\nName: "); nome = Console.ReadLine();
+                Console.Write("Email: "); email = Console.ReadLine();
+                Console.Write("Room: "); numeroapt = int.Parse(Console.ReadLine());
+                a[numeroapt] = new Alocacao { Nome = nome, Email = email };
+
+            }
+            Console.WriteLine("\nBusy rooms:");
+            for (int i = 0; i < a.Length; i++) {
+                if (a[i] != null) {
+                    Console.WriteLine(i.ToString() + ": " + a[i]);
+                }
+            }
+        }
+    }
+}

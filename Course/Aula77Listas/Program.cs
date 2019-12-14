6c5fd767f4fd7ea47d02e2a9ae016b41b27a8fc4
Wallace Vidal <wallacevff@hotmail.com>
2019-12-14 11:24:34 -0300

    Aula 77 Listas (Versão Final)

--- Course/Aula77Listas/Program.cs
+++ Course/Aula77Listas/Program.cs
@@ -9,11 +9,11 @@ namespace Aula77Listas {
             list.Add("Andressa");
             list.Add("Ricardo");
             list.Add("Dayse");
+            list.Add("Armando");
 
             list.Insert(2, "Xulepa");
-
             foreach (string obj in list) {
-                Console.WriteLine(obj);
+                Console.WriteLine("[" + list.FindIndex(x => x == obj) + "] => " + obj);
             }
 
             Console.WriteLine("\nList count: " + list.Count);
@@ -21,7 +21,51 @@ namespace Aula77Listas {
             string s1 = list.Find(x => x[0] == 'A');
             Console.WriteLine("First 'A': " + s1);
 
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
 
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position  'A': " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position  'A': " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 7);
+            Console.WriteLine("\n________________________________________");
+
+            foreach(string obj in list2) {
+                Console.WriteLine("[" + list.FindIndex(x => x == obj) + "] => " + obj);
+            }
+
+            list.Remove("Xulepa");
+            Console.WriteLine( "\n________________________________________");
+            foreach (string obj in list) {
+                Console.WriteLine("[" + list.FindIndex(x => x == obj) + "] => " + obj);
+            }
+
+            list.RemoveAll(x => x[0] == 'A');
+            Console.WriteLine("\n________________________________________");
+            foreach (string obj in list) {
+                Console.WriteLine("[" + list.FindIndex(x => x == obj) + "] => " + obj);
+            }
+
+            list.RemoveAt(0);
+            Console.WriteLine("\n________________________________________");
+            foreach (string obj in list) {
+                Console.WriteLine("[" + list.FindIndex(x => x == obj) + "] => " + obj);
+            }
+
+            list.Add("Wallace");
+            list.Add("Andressa");
+            list.Add("Ricardo");
+            list.Add("Dayse");
+            list.Add("Armando");
+
+            list.RemoveRange(0,6);
+            Console.WriteLine("\n________________________________________");
+            foreach (string obj in list) {
+                Console.WriteLine("[" + list.FindIndex(x => x == obj) + "] => " + obj);
+            }
         }
         
     }

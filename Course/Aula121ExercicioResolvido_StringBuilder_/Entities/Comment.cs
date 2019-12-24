634c71bab3b2cab8987ec825da205b1a94ba1ba1
Wallace Vidal <wallacevff@hotmail.com>
2019-12-24 16:40:39 -0300

    Aula 121 - Exercício Resolvido StringBuilder

---
+++ Course/Aula121ExercicioResolvido_StringBuilder_/Entities/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula121ExercicioResolvido_StringBuilder_.Entities {
+    class Comment {
+        public string text { get; set; }
+
+        public Comment() {
+
+        }
+
+        public Comment(string text) {
+            this.text = text;
+        }
+    }
+}

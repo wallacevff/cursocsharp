8515cd9ac51839397707547fd96f8f4c147e4cf6
Wallace Vidal <wallacevff@hotmail.com>
2020-01-12 16:12:14 -0300

    Aula 130 - 131 Exercicio Resolvido

---
+++ Course/Aula130ExercicioResolvido(Polimorfismo)/Entities/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Aula130ExercicioResolvido_Polimorfismo_.Entities {
+    class Employee {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() {
+
+        }
+
+        public Employee(string name, int hours, double valueperhour) {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valueperhour;
+        }
+
+        public virtual double Payment() {
+            return Hours * ValuePerHour;
+        }
+
+        public override string ToString() {
+            return Name + " - $ " + Payment().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Aula130ExercicioResolvido(Polimorfismo)/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Aula130ExercicioResolvido_Polimorfismo_.Entities {
+    class OutsourcedEmployee  : Employee{
+        public double AddicionalCharge  { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valueperhour, double addicionalCharge) : base( name, hours, valueperhour) {
+            AddicionalCharge = addicionalCharge;
+        }
+
+        public override double Payment() {
+            return base.Payment() + 1.1 * AddicionalCharge;
+        }
+    }
+}

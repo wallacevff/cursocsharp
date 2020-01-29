2d51e14fc3cb8679ad685391b0f4a0a85b1b9ac9
Wallace Vidal <wallacevff@hotmail.com>
2020-01-29 20:29:11 -0300

    Aula 143 - Excessões Personalizadas - PATE 1

---
+++ Course/Aula142ExcessoesPersonalizadas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Aula142ExcessoesPersonalizadas.Entities;
+namespace Aula142ExcessoesPersonalizadas {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+            if (checkOut <= checkIn) {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            } else {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation.ToString());
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now) {
+                    Console.WriteLine("Error in reservation: Reservation dates fot update must be future dates");
+                } else if (checkOut <= checkIn) {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                } else {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+
+            }
+
+
+
+            
+        }
+    }
+}

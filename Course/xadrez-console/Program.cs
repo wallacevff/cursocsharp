e77bb8cc92f35e2672807db63291628420138423
Wallace Vidal <wallacevff@hotmail.com>
2020-02-04 14:57:43 -0300

    Começando a Implementar o Tabluleiro

---
+++ Course/xadrez-console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            
+
+        }
+    }
+}

691373c25192207f3dd58af0401fde5c1c285942
Wallace Vidal <wallacevff@hotmail.com>
2020-02-16 16:39:28 -0300

    Começando a Implementar o Tabuleiro

--- Course/xadrez-console/Program.cs
+++ Course/xadrez-console/Program.cs
@@ -6,8 +6,10 @@ namespace xadrez_console {
         static void Main(string[] args) {
 
             Tabuleiro tab = new Tabuleiro(8,8);
+            Tela.imprimirTabluleiro(tab);
 
-            
+
+
 
         }
     }
--- Course/xadrez-console/tabuleiro/Tabuleiro.cs
+++ Course/xadrez-console/tabuleiro/Tabuleiro.cs
@@ -9,10 +9,13 @@ namespace tabuleiro {
         private Peca[,] pecas;
 
         public Tabuleiro(int linhas, int colunas) {
-            this.linhas = linhas;
+            this.linhas = linhas;  
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca(int linha, int coluna) {
+            return pecas[linha, coluna];
+        }
     }
 }

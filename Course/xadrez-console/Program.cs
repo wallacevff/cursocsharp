3ccb2ae55aa876b62c2d1aeab5010c16bf4c9714
Wallace Vidal <wallacevff@hotmail.com>
2020-02-20 15:21:50 -0300

    Tratando erros com excessões personalizadas

--- Course/xadrez-console/Program.cs
+++ Course/xadrez-console/Program.cs
@@ -7,13 +7,16 @@ namespace xadrez_console {
         static void Main(string[] args) {
 
             Tabuleiro tab = new Tabuleiro(8,8);
-           
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-
-            Tela.imprimirTabluleiro(tab);
+            try {
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                Tela.imprimirTabluleiro(tab);
+            } catch(TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
 
 
         }
--- Course/xadrez-console/tabuleiro/Posicao.cs
+++ Course/xadrez-console/tabuleiro/Posicao.cs
@@ -1,15 +1,15 @@
 ﻿namespace tabuleiro {
     class Posicao {
-        public int Linha { get; set; }
-        public int Coluna { get; set; }
+        public int linha { get; set; }
+        public int coluna { get; set; }
 
         public Posicao(int linha, int coluna) {
-            this.Linha = linha;
-            this.Coluna = coluna;
+            this.linha = linha;
+            this.coluna = coluna;
         }
 
         public override string ToString() {
-            return "(" + this.Linha + ", " + this.Coluna + ")";
+            return "(" + this.linha + ", " + this.coluna + ")";
         }
     }
 }

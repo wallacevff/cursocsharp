610c2674fa4f66f81775b12969c94059907c2fde
Wallace Vidal <wallacevff@hotmail.com>
2020-02-29 21:06:07 -0300

    Jogada especial promoção

--- Course/xadrez-console/xadrez/PartidaDeXadrez.cs
+++ Course/xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -120,6 +120,21 @@ namespace xadrez {
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao) {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)) {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual))) {
                 xeque = true;
             } else {
@@ -132,7 +147,7 @@ namespace xadrez {
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             // #jodadaespecial en passant
 

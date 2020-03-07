9a0a958225a642e7922bbad1edafd5ccfbde3313
Wallace Vidal <wallacevff@hotmail.com>
2020-03-07 13:35:07 -0300

    Aula 184  - File Reader & File Stream

---
+++ Course/Aula183FileFileInfoIOException/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Aula183FileFileInfoIOException {
+    class Program {
+        static void Main(string[] args) {
+            string sourcePath = @"C:\Users\walla\Git\cursocsharp\Course\Aula183FileFileInfoIOException\file1.txt";
+            string targetPath = @"C:\Users\walla\Git\cursocsharp\Course\Aula183FileFileInfoIOException\file2.txt";
+
+            try {
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines) {
+                    Console.WriteLine(line);
+                }
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+}
---
+++ Course/Aula184FileStreamEStreamReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Aula184FileStreamEStreamReader {
+    class Program {
+        static void Main(string[] args) {
+
+            string path = @"C:\Users\walla\Git\cursocsharp\Course\Aula184FileStreamEStreamReader\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            } catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
+            }
+        }
+    }
+}

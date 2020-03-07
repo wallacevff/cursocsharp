91238bde676c6b203b451f58e4fe4a9397655760
Wallace Vidal <wallacevff@hotmail.com>
2020-03-07 13:37:57 -0300

    Aula 184 - Otimizada com File e While

--- Course/Aula184FileStreamEStreamReader/Program.cs
+++ Course/Aula184FileStreamEStreamReader/Program.cs
@@ -6,21 +6,23 @@ namespace Aula184FileStreamEStreamReader {
         static void Main(string[] args) {
 
             string path = @"C:\Users\walla\Git\cursocsharp\Course\Aula184FileStreamEStreamReader\file1.txt";
-            FileStream fs = null;
+            
             StreamReader sr = null;
 
             try {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
             } catch (IOException e) {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
             finally{
                 if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
+               
             }
         }
     }

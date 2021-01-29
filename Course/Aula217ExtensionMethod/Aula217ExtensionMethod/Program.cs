763edb6d1960a3b451d03dfb6a9bb31fa912ce68
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-28 22:53:33 -0300

    Aula 217 - Exercicio Demo 2 Adicionado

--- Course/Aula217ExtensionMethod/Aula217ExtensionMethod/Program.cs
+++ Course/Aula217ExtensionMethod/Aula217ExtensionMethod/Program.cs
@@ -7,8 +7,20 @@ namespace Aula217ExtensionMethod
     {
         static void Main(string[] args)
         {
-            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
-            Console.WriteLine(dt.ElapsedTime());
+
+            Console.Write("Escolha o Exercicio Demo [1 ou 2]: ");
+            int opt = int.Parse(Console.ReadLine());
+            if (opt == 1)
+            {
+                DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+                Console.WriteLine(dt.ElapsedTime());
+            }
+            else if (opt == 2)
+            {
+                String s1 = "Good morning dear students!";
+                Console.WriteLine(s1.Cut(10));
+            }
+            else {; }
         }
     }
 }

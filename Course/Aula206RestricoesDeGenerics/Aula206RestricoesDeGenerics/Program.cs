bf2b509376f11747922bea2f6eb5239510ff7634
Wallace Vidal <wallvff@gmail.com>
2021-01-29 18:49:19 -0300

    Aula 206 - Restrições de Generics

---
+++ Course/Aula206RestricoesDeGenerics/Aula206RestricoesDeGenerics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula206RestricoesDeGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Course/Aula206RestricoesDeGenerics/Aula206RestricoesDeGenerics/Services/CalculationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula206RestricoesDeGenerics.Services
+{
+    class CalculationService
+    {
+        
+    }
+}

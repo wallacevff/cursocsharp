83222590e9dcdd12cee8ad2c3570d04d393af2df
Wallace Vidal <wallvff@gmail.com>
2021-01-29 15:02:27 -0300

    Aula 204-205: Generics

---
+++ Course/Aula204-205Generics/Aula204-205Generics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Aula204_205Generics.Services;
+
+namespace Aula204_205Generics
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            PrintService<string> printService = new PrintService<string>();
+            Console.WriteLine("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}

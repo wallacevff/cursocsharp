83f89081030aef7f2808d51b52ba16cba305c7f1
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-30 23:50:11 -0300

    Aula 222 - Multcast Delegates

---
+++ Course/Aula222MultcastDelegates/Aula222MultcastDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Aula222MultcastDelegates.Services;
+namespace Aula222MultcastDelegates
+{
+    delegate void BinayNumecOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinayNumecOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            op.Invoke(a, b);
+
+        }
+    }
+}
---
+++ Course/Aula222MultcastDelegates/Aula222MultcastDelegates/Services/CalculationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Aula222MultcastDelegates.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+
+        public static void ShowMax(double x, double y)
+        {
+            double max = Max(x, y);
+            System.Console.WriteLine(max);
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = Sum(x, y);
+            System.Console.WriteLine(sum);
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}

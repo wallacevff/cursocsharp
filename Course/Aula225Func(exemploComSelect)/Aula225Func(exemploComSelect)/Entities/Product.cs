d33531bd7dec02614336361f455e210c5a33b138
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-31 08:48:39 -0300

    Aula 225 - Func (exemplo com Select): 1ª versão

---
+++ Course/Aula225Func(exemploComSelect)/Aula225Func(exemploComSelect)/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Aula225Func_exemploComSelect_.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string str)
+        {
+            string[] s = str.Split(",");
+            Name = s[0];
+            Price = double.Parse(s[1],CultureInfo.InvariantCulture);
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Aula225Func(exemploComSelect)/Aula225Func(exemploComSelect)/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Fazer um programa que, a partir de uma lista de produtos, gere uma nova lista contendo os nomes em caixa alta.
+ */
+using System;
+using System.Collections.Generic;
+using Aula225Func_exemploComSelect_.Entities;
+using System.Linq;
+namespace Aula225Func_exemploComSelect_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV,900.00"));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet,350.50"));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+        
+    }
+}

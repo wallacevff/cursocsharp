56f797a2511e045cca47cb9b4791070123da8fd5
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-30 23:38:27 -0300

    Aula 221 - Introdução a Delegates

---
+++ Course/Aula221IntroducaoADelegates/Aula221IntroducaoADelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Aula221IntroducaoADelegates.Services;
+namespace Aula221IntroducaoADelegates
+{
+    delegate double BinayNumecOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinayNumecOperation op = CalculationService.Sum;
+            double result = op(a,b);
+
+            Console.WriteLine(result);
+        }
+    }
+}

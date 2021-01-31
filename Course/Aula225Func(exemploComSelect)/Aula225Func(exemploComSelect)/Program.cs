032b12011f2677e461a6f580b4de0fe46f81bcab
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-31 09:33:43 -0300

    Aula 226 - Introdução ao LINQ

--- Course/Aula225Func(exemploComSelect)/Aula225Func(exemploComSelect)/Program.cs
+++ Course/Aula225Func(exemploComSelect)/Aula225Func(exemploComSelect)/Program.cs
@@ -17,7 +17,7 @@ namespace Aula225Func_exemploComSelect_
             list.Add(new Product("Tablet,350.50"));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = NameUpper;
+            Func<Product, string> func = p => p.Name.ToUpper();
 
             List<string> result = list.Select(func).ToList();
             foreach (string s in result)
@@ -26,10 +26,7 @@ namespace Aula225Func_exemploComSelect_
             }
 
         }
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
-        }
+       
         
     }
 }
---
+++ Course/Aula226IntroducaoAoLINQ/Aula226IntroducaoAoLINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Aula226IntroducaoAoLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression
+            IEnumerable<int> result = numbers.Where(n => n % 2 == 0).Select(n => n * 10);
+            
+            //Execute the query            
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}

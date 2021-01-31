e91af9ba0034b101a6e2e77b6e6edb42df27d666
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-31 08:31:58 -0300

    Aula 224 - Action (exemplo com ForEach): 1ª versão

---
+++ Course/Aula224Action(exemploComForEach)/Aula224Action(exemploComForEach)/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Fazer um programa que, a partir de uma lista de produtos, aumente o preço dos produtos em 10%
+ */
+using System;
+using System.Collections.Generic;
+using Aula224Action_exemploComForEach_.Entities;
+
+namespace Aula224Action_exemploComForEach_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV,900.00"));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet,350.50"));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.ForEach(UpdatePrice);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+        static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
+        }
+    }
+}

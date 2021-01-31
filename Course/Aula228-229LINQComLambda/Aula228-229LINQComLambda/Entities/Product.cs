cc772982c9c546aaf31e60020c4ed54855e750a8
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-31 11:37:07 -0300

    Aulas 228 - 229: LINQ com Lambda

---
+++ Course/Aula228-229LINQComLambda/Aula228-229LINQComLambda/Entities/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Aula228_229LINQComLambda.Entities
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public Product(int id, string name, double price, Category category)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Category = category;
+        }
+
+        public Product()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;
+        }
+    }
+}

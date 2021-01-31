cef45102e89e85774f5180a5f11148b4880b95d9
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-01-30 21:35:59 -0300

    Aula 219 - Função Lambda

--- Course/Aula219UmaExperienciaComComparison/219UmaExperienciaComComparison/Program.cs
+++ Course/Aula219UmaExperienciaComComparison/219UmaExperienciaComComparison/Program.cs
@@ -12,19 +12,14 @@ namespace Aula219UmaExperienciaComComparison
 
             list.Add(new Product("TV", 900.00));
             list.Add(new Product("Notebook", 1200.00));
-            list.Add(new Product("Tablet", 450.00));            
+            list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
-            list.Sort(comp);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
         }
 
-        static int CompareProducts(Product p1, Product p2)
-        {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-        }
     }
 }
